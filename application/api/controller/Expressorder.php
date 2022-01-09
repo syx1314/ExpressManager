4bbac994d3c99d21c870fe2403351eb271c14a04
syx1314 <6944073asd>
2022-01-09 23:43:38 +0800

    qbd 接口 和快递接口编写

--- application/api/controller/Expressorder.php
+++ application/api/controller/Expressorder.php
@@ -5,6 +5,7 @@ namespace app\api\controller;
 use app\common\library\Createlog;
 use app\common\model\Client;
 use app\common\model\Porder as PorderModel;
+use Recharge\Qbd;
 
 class Expressorder
 {
@@ -27,7 +28,58 @@ class Expressorder
     }
     //估算运费
     public function estimateprice() {
+        $weight = I('weight');
+        $qbd= new Qbd();
+        $priceRes = $qbd->findPrice($weight,'河北省承德市隆化县隆化镇下洼子南胡同盛达水暖','广东省东莞市横沥镇新世纪华庭金岸7A','6');
+        var_dump($priceRes);
+        if ($priceRes['errno']==0) {
+            // 看折扣 还是看 首重续重 计算
+            $data= $priceRes['data'];
+            return   $this->totalPrice($weight,$priceRes['data']['priceA'],$data['priceB'],$data['discount'],$data['totalFeeOri']);
+        }else {
+            return $priceRes['msg'];
+        }
 
+    }
+
+    /**
+     * 预估价格
+     * @param $weight //重量
+     * @param $priceA //首重
+     * @param $priceB //续重
+     * @param $discount //折扣
+     * @param $totalFeeOri //折扣原价
+     * @return float|int //计算出来的总价
+     */
+    private function totalPrice($weight,$priceA,$priceB,$discount,$totalFeeOri) {
+        // 查询价格 根据价格计算 保价费 重量  续重 或者 总价x折扣
+        if ($priceA) {
+            if ($priceB<2.5) {
+                $priceB =2.5;
+            }elseif ($priceB<3) {
+                $priceB =3;
+            }else {
+                $priceB= $priceB+0.5;
+            }
+            // 首重 加 续重 算法
+            if ($priceA<6.5) {
+                return  6.5+($weight-1)*$priceB;
+            }else if ($priceA<7) {
+                return  7+($weight-1)*$priceB;
+            }else if ($priceA<8) {
+                return  8+($weight-1)*$priceB;
+            }else {
+                return  ($priceA+0.5)+($weight-1)*$priceB;
+            }
+        }else{
+            if ($discount<7) {
+                return $totalFeeOri*0.7;
+            }else if ($discount<8){
+                return $totalFeeOri*0.8;
+            }else {
+                return  $totalFeeOri*($discount+0.5)*0.1;
+            }
+        }
     }
 
 
@@ -44,8 +96,31 @@ class Expressorder
     }
     // 地址解析
     public function nlpAddress() {
-
+     $qbd= new Qbd();
+      //  "data":{
+//                "province":"浙江省",
+//                "province_code":"330000",
+//                "provinceId":330000,
+//                "city":"杭州市",
+//                "city_code":null,
+//                "cityId":330100,
+//                "county":"余杭区",
+//                "county_code":null,
+//                "countyId":330110,
+//                "town":"闲林街道",
+//                "townId":330110011,
+//                "town_code":null,
+//                "detail":"竹海水韵竹邻间12幢1单元302室",
+//                "person":"陈佳佳",
+//                "phonenum":"15658102016",
+//                "mobile":null,
+//                "telPhone":null
+//                 }
+    return $qbd->nlpaddress(I('address'));
     }
+
+
+
     /**
      * 充值记录
      */
--- extend/Recharge/Qbd.php
+++ extend/Recharge/Qbd.php
@@ -12,24 +12,43 @@ use think\Log;
  **/
 class Qbd
 {
-    private $userid;
-    private $apikey;
-    private $notify;
-    private $apiurl;//充值接口
 
-    public function __construct($option)
-    {
-        $this->userid = isset($option['param1']) ? $option['param1'] : '';
-        $this->apikey = isset($option['param2']) ? $option['param2'] : '';
-        $this->notify = isset($option['param3']) ? $option['param3'] : '';
-        $this->apiurl = isset($option['param4']) ? $option['param4'] : '';
-    }
 
     /**
-     * 创建订单
+     * @param $orderSendTime  //预约时间
+     * @param $senderText  //寄件人文本
+     * @param $receiveText // 收件人文本
+     * @param $packageNum  // 包裹书
+     * @param $senderName  //寄件人名字
+     * @param $senderCity  //寄件人城市
+     * @param $senderAddress //寄件人地址
+     * @param $senderPhone   //寄件人手机号
+     * @param $receiveName  // 收件人名字
+     * @param $receiveAddress // 收件人地址
+     * @param $receiveCity  // 收件人城市
+     * @param $receivePhone // 收件人手机号
+     * @param $weight // 重量
+     * @param $goods  // 商品名字
+     * @param $insuredValue
+     * @param $guaranteeValueAmount
+     * @param $remark
+     * @param $channelName // 渠道名字
+     * @param $priceA  // 首重
+     * @param $priceB  // 续重
+     * @param $discount // 折扣
+     * @param $fee
+     * @param $isThird
+     * @param $isInsured
+     * @param $fee1
+     * @param $remark1
+     * @param $type // 快递类型
+     * @param $sadd
+     * @param $agentCode
      */
-    public function createOrder($data)
-    {
+    public function createOrder($orderSendTime,$senderText,$receiveText,$packageNum,$senderName,$senderCity,
+                                $senderAddress,$senderPhone,$receiveName,$receiveAddress,$receiveCity,$receivePhone,
+                                $weight,$goods,$insuredValue,$guaranteeValueAmount,$remark,$channelName,
+                                $priceA,$priceB,$discount,$fee,$isThird,$isInsured,$fee1,$remark1,$type,$sadd,$agentCode){
         $data = [
                 "t" => $this->timeram(),
                 "orderSendTime" => "",
@@ -72,7 +91,42 @@ class Qbd
 
     // 地址解析
     public function nlpaddress($text) {
-        return $this->http('hhttps://jdkd.ulifego.com/ht/common/nlpaddress?t='.$this->timeram().'&text='.$text.'&type=1',null);
+//        {
+//            "msg":"success",
+//            "code":0,
+//            "data":{
+//                "province":"浙江省",
+//                "province_code":"330000",
+//                "provinceId":330000,
+//                "city":"杭州市",
+//                "city_code":null,
+//                "cityId":330100,
+//                "county":"余杭区",
+//                "county_code":null,
+//                "countyId":330110,
+//                "town":"闲林街道",
+//                "townId":330110011,
+//                "town_code":null,
+//                "detail":"竹海水韵竹邻间12幢1单元302室",
+//                "person":"陈佳佳",
+//                "phonenum":"15658102016",
+//                "mobile":null,
+//                "telPhone":null
+//                 }
+//        }
+        $res= $this->http('https://jdkd.ulifego.com/ht/common/nlpaddress?t='.$this->timeram().'&text='.urlencode($text).'&type=1',null);
+        if ($res['errno']== 0 && $res['data']) {
+            return rjson(0,'解析地址成功',$res['data']);
+        }else {
+            return rjson(1,'解析地址出错',$res['data']);
+        }
+
+    }
+
+    //快递黑名单
+    public function expressblacklist() {
+//        {"msg":"success","code":0,"isExist":false}
+//        http://jdkd.ulifego.com/ht/express/expressblacklist/checkMobile/15658102016?t=1641571171922
     }
     //timeram
     public function timeram() {
@@ -90,10 +144,41 @@ class Qbd
             'weight'=>$weight,
             'sendAdd'=>$sendAdd,
             'reviceAdd'=>$reviceAdd,
-            'type'=>$type,
+            'type'=>$type, // 快递
         ];
-
-        return $this->http_get('https://jdkd.ulifego.com/ht/common/getChannelFreight',$data);
+//        {
+//            "msg":"success",
+//            "billChannel":Object{...},
+//            "code":0,
+//            "data":{
+//                "type":2,
+//                "typeName":"德邦快递",
+//                "name":"QBD-德邦-折扣-特快",
+//                "fee":11,
+//                "fee1":7.04,
+//                "serviceCharge":0,
+//                "priceA":0,
+//                "priceB":0,
+//                "discount":6.4,
+//                "takeRate":0,
+//                "isThird":false,
+//                "isInsured":true,
+//                "totalFeeOri":11,
+//                "remark":"与官网价格一致，请按实际重量填写，系统才能匹配最低价渠道 （拒收，原价，长超150cm,长宽高超250cm,需20元加长费）特别声明：德邦2KG-4KG，有的地方4-3KG比2kg便宜，一切以德邦官网价格为准，抛比8000",
+//                "takeFee":0,
+//                "rate":null,
+//                "insuredValue":null,
+//                "guarantFee":0,
+//                "totalFee":7.04,
+//                "otherFee":0,
+//                "otherRemark":null
+//            }
+//        }
+        $res=$this->http('https://jdkd.ulifego.com/ht/common/getChannelFreight',$data);
+        if ($res['data']) {
+            return rjson(0,'费用预估成功',$res['data']);
+        }
+        return rjson(1,'费用预估失败',$res['data']);
     }
 
     /**
@@ -105,23 +190,33 @@ class Qbd
     }
 
     /**
+     * 取消订单
+     */
+    public function cancelOrder() {
+
+    }
+
+    /**
      * 登录存token
      */
-    public function login($account,$pwd)
+    public function login()
     {
 
         $data = [
-            'account'=>$account,
-            'password'=>$pwd
+            'account'=>'13102101195',
+            'password'=>'123456'
         ];
        $loginRes = $this->http('https://jdkd.ulifego.com/ht/web/login/loginNew',$data);
-       if ($loginRes['code'] == 0) {
-          $loginData= $loginRes['code']['"data": '];
-           RedisPackage::set('qbdToken',$loginData['token']);
+       if ($loginRes['errno'] == 0) {
+           $loginData= $loginRes['data'];
+           var_dump($loginData);
+           $redis=new RedisPackage();
+           $redis::set('qbdToken',$loginData['token']);
+           return rjson(0,'登录成功');
        }else{
            // 自动登录失败 发送通知消息
+           return rjson(1,'登录失败',$loginRes['errmsg']);
        }
-        // 暂时存个qbdtoken
     }
 
 
@@ -137,19 +232,44 @@ class Qbd
             $strPOST = json_encode($param);
             curl_setopt($oCurl, CURLOPT_POST, true);
             curl_setopt($oCurl, CURLOPT_POSTFIELDS, $strPOST);
+            curl_setopt($oCurl, CURLOPT_HTTPHEADER, ["Content-Type:application/json;charset=utf-8"]);
+            Log::error("请求参数".$strPOST);
         }
         curl_setopt($oCurl, CURLOPT_CONNECTTIMEOUT, 10);
         curl_setopt($oCurl, CURLOPT_TIMEOUT, 30);
         curl_setopt($oCurl, CURLOPT_HEADER, 0);
-        $token = RedisPackage::get('qbdToken');
+        $redis=new RedisPackage();
+        $token = $redis::get('qbdToken');
 //        QBD/3.0(iPhone;iOS 10.3.1;Scale/3.00)
         if ($token) {
-            curl_setopt($oCurl, CURLOPT_HTTPHEADER, ['token:'.$token]);
+            $header= [
+                'Content-Type'=>'application/json;charset=utf-8',
+                'token' => $token
+            ];
+            curl_setopt($oCurl, CURLOPT_HTTPHEADER, $header);
+        }else if (!strpos($url,'loginNew')){
+            // 不是登录路径 而且没有登录成功 就去登录
+            $this->login();
+            return rjson(1,'系统繁忙,请重试','');
         }
-        Log::error($strPOST);
         $sContent = curl_exec($oCurl);
         curl_close($oCurl);
-        return json_decode($sContent, true);
+        Log::error("快递接口请求地址".$url);
+        Log::error("快递接口返回".$sContent);
+        $res=json_decode($sContent, true);
+        if ($res) {
+            if ( $res['code']== 0) {
+                return rjson(0,'接口请求成功',$res['data']);
+            }else if ($res['code']== 401) {
+                // token 失效 继续登录
+                $this->login();
+                return rjson(1,'系统繁忙,请重试',$res['msg']);
+            }else{
+                return rjson($res['code'],'请求失败原因:'.$res['msg'],$res['msg']);
+            }
+        }else{
+            return rjson(1,'系统异常接口失败','');
+        }
 
     }
 }
